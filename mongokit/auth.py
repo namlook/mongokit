e40a61dc12a90d56618bed99ac23f3acfee0f674
namlook@nichjo <namlook@nichjo>
2010-01-04 16:20:44 +0100

    migrating auth.py

--- mongokit/auth.py
+++ mongokit/auth.py
@@ -25,10 +25,10 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from mongokit import MongoDocument
+from mongokit import Document
 import hashlib, os
 
-class User(MongoDocument):
+class User(Document):
     structure = {
         "_id":unicode,
         "user":{
