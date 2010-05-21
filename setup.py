331a78ec2d5782778396f742baba8bb2afb6d618
Namlook <devnull@localhost>
2010-05-21 16:41:43 +0200

    prepare release v0.5.6

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ import codecs
 
 setup(
     name = 'mongokit',
-    version = '0.5.5',
+    version = '0.5.6',
 
     description = 'Python mongodb kit',
     long_description = codecs.open('README', "r", "utf-8").read(),
