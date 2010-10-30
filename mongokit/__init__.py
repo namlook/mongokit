89bd1d0b75106dd0f5b62292eb38d7c09ec68004
Namlook <n.namlook@gmail.com>
2010-10-30 15:21:36 +0200

    prepare release v0.5.12

--- mongokit/__init__.py
+++ mongokit/__init__.py
@@ -25,7 +25,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = "0.5.11"
+__version__ = "0.5.12"
 
 from pymongo.dbref import DBRef
 from cursor import Cursor
