fda3315ae99e275dbd5a1caf294dc4f5a949d866
Namlook <n.namlook@gmail.com>
2010-11-07 13:51:33 +0100

    wtf ?git commit -am fix

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ import codecs
 
 setup(
     name = 'mongokit',
-    version = 'O.5.13', # TODO don't forget to change version
+    version = '0.5.13', # TODO don't forget to change version
 
     description = 'Python mongodb kit',
     long_description = codecs.open('README', "r", "utf-8").read(),
