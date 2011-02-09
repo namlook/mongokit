5e3d60d58cdf91bc3b618bf9a2125ec7519b78a4
Namlook <n.namlook@gmail.com>
2011-02-09 12:53:06 +0100

    bump v0.6

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ import codecs
 
 setup(
     name = 'mongokit',
-    version = '0.5.13.1', # TODO don't forget to change version
+    version = '0.6', # TODO don't forget to change version
 
     description = 'Python mongodb kit',
     long_description = codecs.open('README', "r", "utf-8").read(),
@@ -47,7 +47,7 @@ setup(
     url = 'http://namlook.github.com/mongokit/',
 
     classifiers = [
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Other Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
