3db4eecb1fc13dfe146f1563d92795666cd81720
Namlook <n.namlook@gmail.com>
2011-02-25 11:54:11 +0100

    We are in 2011 (two years already)

--- doc/conf.py
+++ doc/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MongoKit'
-copyright = u'2009-2010, Namlook'
+copyright = u'2009-2011, Nicolas Clairon'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
