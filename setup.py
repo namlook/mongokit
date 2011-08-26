18988531a39cb6dbda245f72ab60bb47fc9ddc1d
Namlook <n.namlook@gmail.com>
2011-08-26 10:45:09 +0200

    bump v0.7.1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ import codecs
 
 setup(
     name = 'mongokit',
-    version = '0.7', # TODO don't forget to change version
+    version = '0.7.1', # TODO don't forget to change version
 
     description = 'Python mongodb kit',
     long_description = codecs.open('README', "r", "utf-8").read(),
@@ -62,7 +62,7 @@ setup(
     zip_safe = False,
 
     install_requires = [
-        'pymongo>=1.9',
+        'pymongo>=2.0.1',
         'anyjson>=0.2.2',
     ]
 
