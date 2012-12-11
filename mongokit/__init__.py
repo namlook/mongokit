f73316bf369bccbff1bd43055d1f28cd485f3e90
namlook <n.namlook@gmail.com>
2012-12-11 15:04:01 +0100

    bump v0.8.2

--- mongokit/__init__.py
+++ mongokit/__init__.py
@@ -25,7 +25,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 
 from bson.dbref import DBRef
 from cursor import Cursor
