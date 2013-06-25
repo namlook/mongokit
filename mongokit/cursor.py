namlook/mongokit

commit 8e350ccf727845b2022315c0716b7724230c4391

support pymongo 2.3

mongokit/cursor.py
@@ -26,6 +26,7 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from pymongo.cursor import Cursor as PymongoCursor
+from collections import deque
 
 class Cursor(PymongoCursor):
     def __init__(self, *args, **kwargs):
@@ -35,14 +36,24 @@ class Cursor(PymongoCursor):
         super(Cursor, self).__init__(*args, **kwargs)
 
     def next(self):
-        if self.__empty:
+        if self._Cursor__empty:
             raise StopIteration
         db = self._Cursor__collection.database
         if len(self.__data) or self._refresh():
-            next = db._fix_outgoing(self._Cursor__data.pop(0), self._Cursor__collection, wrap=self.__wrap)
+            if isinstance(self._Cursor__data, deque):
+                item = self._Cursor__data.popleft()
+            else:
+                item = self._Cursor__data.pop(0)
+            if self._Cursor__manipulate:
+                son = db._fix_outgoing(item, self._Cursor__collection)
+            else:
+                son = item
+            if self.__wrap is not None:
+                return self.__wrap(son, collection=self._Cursor__collection)
+            else:
+                return son
         else:
             raise StopIteration
-        return next
 
     def __getitem__(self, index):
         obj = super(Cursor, self).__getitem__(index)

mongokit/database.py
@@ -45,37 +45,14 @@ class Database(PymongoDatabase):
                 self._collections[key] = Collection(self, key) 
             return self._collections[key]
 
-
     def dereference(self, dbref, model = None):
         if model is None:
           return super(Database, self).dereference(dbref)
-
         if not isinstance(dbref, DBRef):
             raise TypeError("first argument must be a DBRef")
-
         if dbref.database is not None and dbref.database != self.name:
-            raise ValueError("dereference must be called on the database `%s`" % dbref.database)
-
+            raise ValueError("trying to dereference a DBRef that points to "
+                             "another database (%r not %r)" % (dbref.database, self._Database__name))
         if not issubclass(model, Document):
             raise TypeError("second argument must be a Document")
-
         return getattr(self[dbref.collection], model.__name__).one({'_id': dbref.id})
-
-    def _fix_outgoing(self, son, collection, wrap=None):
-        """Apply manipulators to a SON object as it comes out of the database.
-
-        :Parameters:
-          - `son`: the son object coming out of the database
-          - `collection`: the collection the son object was saved in
-          - `wrap` : a class object which its __init__ take a SON object and a collection
-
-          If `wrap` is not None, return an instance of the wrap object. Return
-          a SON object otherwise.
-        """
-        son = super(Database, self)._fix_outgoing(son, collection)
-        if wrap is not None:
-            if wrap.type_field in son:
-                return getattr(collection, son[wrap.type_field])(son)
-            return wrap(son, collection=collection)
-        return son
-

commit fa07d9796ca8f14e473be7b9c39b613fa44e759e

Fix inherited queries (issue #106)
The commit 8e350cc broke support for
inherited queries. This fixes the issue.

Note that a query result will be cast to whichever registered type
its type_field indicates, not just those which are subtypes
of the cursor's collection.

Credit to effem-git@GitHub for the code.

mongokit/cursor.py
@@ -49,6 +49,9 @@ class Cursor(PymongoCursor):
             else:
                 son = item
             if self.__wrap is not None:
+                if self.__wrap.type_field in son:
+                    return getattr(self._Cursor__collection,
+                                   son[self.__wrap.type_field])(son)
                 return self.__wrap(son, collection=self._Cursor__collection)
             else:
                 return son