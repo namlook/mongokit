c6fe42f301b591e64e88b8551b43696c2aea5f47
namlook <n.namlook@gmail.com>
2013-08-08 13:18:18 +0200

    fix #131 - Use PEP8 recommendation for import

--- mongokit/__init__.py
+++ mongokit/__init__.py
@@ -39,13 +39,14 @@ from collection import Collection
 from connection import Connection, MongoClient
 from master_slave_connection import MasterSlaveConnection
 from replica_set_connection import ReplicaSetConnection
-from pymongo import ASCENDING as INDEX_ASCENDING,\
-  DESCENDING as INDEX_DESCENDING,\
-  ALL as INDEX_ALL,\
-  GEO2D as INDEX_GEO2D,\
-  GEOHAYSTACK as INDEX_GEOHAYSTACK,\
-  GEOSPHERE as INDEX_GEOSPHERE,\
-  OFF as INDEX_OFF, \
+from pymongo import (
+  ASCENDING as INDEX_ASCENDING,
+  DESCENDING as INDEX_DESCENDING,
+  ALL as INDEX_ALL,
+  GEO2D as INDEX_GEO2D,
+  GEOHAYSTACK as INDEX_GEOHAYSTACK,
+  GEOSPHERE as INDEX_GEOSPHERE,
+  OFF as INDEX_OFF,
   HASHED as INDEX_HASHED
+)
 from migration import DocumentMigration
-
