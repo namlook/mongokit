e053e0760b3fbbc1506fc47d4bb6e96fa1939f94
Mieszko <mieszko.chowaniec@gmail.com>
2014-11-23 10:38:12 +0100

    PEP 8 imports
    From PEP8:
     Imports should be grouped in the following order:
    
        standard library imports
        related third party imports
        local application/library specific imports
    
    You should put a blank line between each group of imports.

--- mongokit/auth.py
+++ mongokit/auth.py
@@ -25,9 +25,10 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from mongokit import Document
 import hashlib
 import os
+
+from mongokit import Document
 
 
 class User(Document):
